365224321440669f7a9e1c8bde7c07a07a762e61
mag162 <mag162@alu.ua.es>
2025-03-18 11:12:58 +0100

    51797315G Creación clases CADProduct.cs y ENProduct.cs

---
+++ library/CADProduct.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class CADProduct
+    {
+
+        private string constring;
+
+        public CADProduct()
+        {
+
+        }
+
+        public bool Create(ENProduct en)
+        {
+
+        }
+
+        public bool Update(ENProduct en)
+        {
+
+        }
+
+        public bool Delete(ENProduct en)
+        {
+
+        }
+
+        public bool Read(ENProduct en)
+        {
+
+        }
+
+        public bool ReadFirst(ENProduct en)
+        {
+
+        }
+
+        public bool ReadNext(ENProduct en)
+        {
+
+        }
+
+        public bool ReadPrev(ENProduct en)
+        {
+
+        }
+
+    }
+}
---
+++ library/ENProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    class ENProduct
+    {
+
+        private string _code;
+        private string _name;
+        private int _amount;
+        private float _price;
+        private int _category;
+        private DateTime _creationDate;
+
+        public string Code { set { _code = value; } get { return _code; } }
+        public string Name { set { _name = value; } get { return _name; } }
+
+
+
+    }
+}
